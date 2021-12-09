efaef66bc547e4d5853b544777c5b3d6183f2c35
Art Navsegda <artnavsegda@gmail.com>
2021-12-09 13:30:00 +0300

    Update async2.c

--- src/async2.c
+++ src/async2.c
@@ -3,6 +3,7 @@
 #include <readline/history.h>
 #include <stdlib.h>  /* for free() */
 #include <unistd.h> /* for usleep() */
+#include <poll.h>
 
 char running = 1;
 
@@ -28,14 +29,17 @@ int main()
 {
  const char *prompt = "WOOP> ";
 
+ struct pollfd fd = {0, POLLIN};
+
  // Install the handler
  rl_callback_handler_install(prompt, my_rlhandler);
 
  // Enter the event loop (simple example, so it doesn't do much except wait)
  running = 1;
  while(running){
-    usleep(10000);
-    rl_callback_read_char();
+    //usleep(10000);
+    if(poll(&fd, 1, 0) == 1)
+      rl_callback_read_char();
  };
  printf("\nEvent loop has exited\n");
 
